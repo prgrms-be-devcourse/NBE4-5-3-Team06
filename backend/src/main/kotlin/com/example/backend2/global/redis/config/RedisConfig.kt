2e6fcd14040f69cced17e85da666f0e1d7b90323
joungHyeon <jounghyeon123@daum.net>
2025-04-09 17:49:29 +0900

    feat: 동시성 비즈니스 로직 구현

--- backend/src/main/kotlin/com/example/backend2/global/redis/config/RedisConfig.kt
+++ backend/src/main/kotlin/com/example/backend2/global/redis/config/RedisConfig.kt
@@ -1,4 +1,4 @@
-package com.example.backend2.global.redis
+package com.example.backend2.global.redis.config
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
