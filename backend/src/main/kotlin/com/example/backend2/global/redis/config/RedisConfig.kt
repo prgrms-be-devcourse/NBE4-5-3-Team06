ccdffacbab3e1d4934a728867da4007570e62b1e
ChoHyunWoo <gusdndlek12@naver.com>
2025-04-10 16:08:32 +0900

    ♻️ refactor: 구글 로그인 성공, 프로필 수정 및 저장 성공

--- backend/src/main/kotlin/com/example/backend2/global/redis/config/RedisConfig.kt
+++ backend/src/main/kotlin/com/example/backend2/global/redis/config/RedisConfig.kt
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisPassword
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
@@ -13,12 +14,12 @@ import org.springframework.data.redis.serializer.StringRedisSerializer
 class RedisConfig(
     @Value("\${spring.data.redis.host}") private val host: String,
     @Value("\${spring.data.redis.port}") private val port: Int,
-//    @Value("\${spring.data.redis.password}") private val password: String,
+    @Value("\${spring.data.redis.password}") private val password: String,
 ) {
     @Bean
     fun redisConnectionFactory(): RedisConnectionFactory {
         val configuration = RedisStandaloneConfiguration(host, port)
-//        configuration.setPassword(RedisPassword.of(password))
+        configuration.setPassword(RedisPassword.of(password))
         return LettuceConnectionFactory(configuration)
     }
 
