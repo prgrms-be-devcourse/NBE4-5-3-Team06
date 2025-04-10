93bf786ce0e27343d7a664e89d9838d6c21220de
gwony <moonsky1124@naver.com>
2025-04-10 12:56:22 +0900

    🔧 chore: 관리자 비밀번호 인코딩 한 걸로 변경

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,14 +12,12 @@ INSERT INTO USER_TABLE (USER_UUID, EMAIL, NICKNAME, PASSWORD, CREATED_DATE, MODI
 VALUES
     ('user1', 'user1@example.com', 'AuctionMaster', 'password123', '1990-01-01', '2025-03-05', 'USER'),
     ('user2', 'user2@example.com', 'BidKing', 'password456', '1985-06-15', '2025-03-05', 'USER'),
-    ('admin1', 'admin@example.com', 'AdminUser', 'adminpassword', '1980-12-30', '2025-03-05', 'ADMIN');
-
+    ('Admin1', 'admin@example.com', 'Admin', '$2a$10$lJO1tfsV1baFpqeMZ6msJe0hE4e5fC619Y6dbDsnTglFTtOHYrVui','1985-06-15', '2025-03-05', 'ADMIN' );
 
 -- PRODUCT 테이블
-INSERT INTO PRODUCT_TABLE (PRODUCT_ID, PRODUCT_NAME, IMAGE_URL, DESCRIPTION)
+    INSERT INTO PRODUCT_TABLE (PRODUCT_ID, PRODUCT_NAME, IMAGE_URL, DESCRIPTION)
 VALUES
-    (1, 'Apple MacBook Pro', 'https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/macbook-pro-og-202410?wid=600&hei=315&fmt=jpeg&qlt=95&.v=1728658184478
-', 'Apple의 최신 MacBook Pro 모델입니다.'),
+    (1, 'Apple MacBook Pro', 'https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/macbook-pro-og-202410?wid=600&hei=315&fmt=jpeg&qlt=95&.v=1728658184478', 'Apple의 최신 MacBook Pro 모델입니다.'),
     (2, 'Vintage Watch', 'https://sitem.ssgcdn.com/16/72/26/item/1000598267216_i1_750.jpg', '고급 빈티지 시계.'),
     (3, 'Sony 4K TV', 'https://m.media-amazon.com/images/I/71TGwbLjzAL.jpg', '최신 4K 해상도 Sony TV.');
 
